f3c74abc3e3922d166371912a61c020c819d5f48
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-18 16:09:15 -0400

    commit tree 018d0e6cf18e38d1131a3666681adefab00a357a

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,31 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangePet(type) {
+    setFilters({ type: type });
+  }
+
+  function handleFindPetsClick() {
+    let baseURL = "http://localhost:3001/pets";
+
+    if (filters.type !== "all") {
+      baseURL += `?type=${filters.type}`;
+    }
+
+    fetch(baseURL)
+      .then((res) => res.json())
+      .then((petsArray) => {
+        //console.log(petsArray)
+        setPets(petsArray);
+      });
+  }
+  const onAdoptPet = (id) => {
+    const newPetArray = pets.map((pet) => {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet;
+    });
+    setPets(newPetArray);
+  };
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +40,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangePet={handleChangePet}
+              onFindPetsClick={handleFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
